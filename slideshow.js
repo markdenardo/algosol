064afc224f36ca330278209a34dd3d94764046b9
MDN <mark.denardo@gmail.com>
2022-12-17 12:20:08 -0500

    Create slideshow.js

---
+++ slideshow.js
@@ -0,0 +1,20 @@
+//	solstice slideshow
+//	mdn 2022
+
+//	bh
+s0.initVideo("https://media0.giphy.com/media/YZdNFzYZIEi0cCaSpn/giphy.mp4")
+s1.initVideo("https://media0.giphy.com/media/YZdNFzYZIEi0cCaSpn/giphy.mp4")
+s2.initVideo("https://media2.giphy.com/media/fdd85RxytnbqU58hwP/giphy.mp4")
+s3.initVideo("https://media3.giphy.com/media/FoW0Gxurx5dLwzCqqj/giphy.mp4")
+
+//	ideon
+s0.initVideo("https://media1.giphy.com/media/eUX0LVWUUbtLaAI5HT/giphy.mp4")
+s1.initVideo("https://media0.giphy.com/media/ebONPXiwdiodPpSEN4/giphy.mp4")
+s2.initVideo("https://media3.giphy.com/media/DkleR7V8RMtrim0cH5/giphy.mp4")
+s3.initVideo("https://media3.giphy.com/media/fvCoIEB8YtKghURZcA/giphy.mp4")
+
+//	kiki
+s0.initVideo("https://media2.giphy.com/media/k04WlBzKhpd04/giphy.mp4")
+s1.initVideo("https://media4.giphy.com/media/3ohzdJVHUqgiXIILUQ/giphy.mp4")
+s2.initVideo("https://media3.giphy.com/media/3ohzdD0XoSiqaU8OMo/giphy.mp4")
+s3.initVideo("https://media3.giphy.com/media/3ohzdEQAcSxII2n2HC/giphy.mp4")
