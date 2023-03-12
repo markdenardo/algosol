0251ca47d36dc4c3bddcdb5aeeaf42fcc5dbe17e
MDN <mark.denardo@gmail.com>
2023-03-12 11:59:05 -0400

    Create bh.js

---
+++ bh.js
@@ -0,0 +1,10 @@
+//	bh
+s0.initVideo("https://media0.giphy.com/media/YZdNFzYZIEi0cCaSpn/giphy.mp4")
+s1.initVideo("https://media3.giphy.com/media/5kPbL5CKuaKcysKzpb/giphy.mp4")
+s2.initVideo("https://media2.giphy.com/media/fdd85RxytnbqU58hwP/giphy.mp4")
+s3.initVideo("https://media3.giphy.com/media/FoW0Gxurx5dLwzCqqj/giphy.mp4")
+src(s1)
+	.add(s2)
+	.add(s3)
+	.out(o0)
+render(o0)
