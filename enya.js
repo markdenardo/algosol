1f295535a5b37d75adaa4f81887ab2d5fcde2665
MDN <mark.denardo@gmail.com>
2023-04-23 01:34:15 -0400

    Create enya.js

---
+++ enya.js
@@ -0,0 +1,16 @@
+// licensed with CC BY-NC-SA 4.0 https://creativecommons.org/licenses/by-nc-sa/4.0/
+//who can say ??
+// MDN(@markdenardo)2023
+const f = (() => a.fft[0])
+s0.initVideo('https://media3.giphy.com/media/4ugTknTCM4f0FIQram/giphy.mp4')
+solid(1, 1, 1)
+	.color(0, 0, 1)
+	.mask(src(s0)
+		.pixelate(50, 50))
+	.out(o0)
+render(o0)
+
+src(o0)
+	.blend(src(s0), f)
+	.out(o1)
+render(o1)
